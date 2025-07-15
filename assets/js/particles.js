36714f0ea47d90ecf8442f68b205e149ba12584d
Azmat <156051106+hussain-azmat@users.noreply.github.com>
2025-07-15 13:39:15 +0500

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 400,
+        value: 150,
         density: {
           enable: true,
           value_area: 800
